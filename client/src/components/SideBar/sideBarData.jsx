f32440b37e35b66878bf568b079d81bc8e09616b
Ango <angojeff433@gmail.com>
2020-12-01 04:13:43 +0100

    backend

--- client/src/components/SideBar/sideBarData.jsx
+++
@@ -1,6 +0,0 @@
-export const SideBarData = [
-  { title: "Home", path: "/", cname: "nav_text" },
-  { title: "About", path: "/", cname: "nav_text" },
-  { title: "Developers", path: "/", cname: "nav_text" },
-  { title: "Interns", path: "/", cname: "nav_text" },
-];
